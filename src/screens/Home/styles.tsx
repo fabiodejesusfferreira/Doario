f0295da61857469adbcf58bda67aa40b18b3b076
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-16 18:48:20 -0300

    Estilização para os components

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,10 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
     alignItems: "center",
     width: "100%",
-  }
+  },
+  navContainer: {
+    marginTop: 55,
+    height: 40,
+    margin: 10,
+    width: "97%",
+    alignItems: "center",
+  },
+  indicadoresContainer: {
+    height: 130,
+    marginTop: 15,
+  },
+  buttonBackToTop: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    padding: 5,
+    marginRight: 20,
+    marginBottom: 150,
+    borderRadius: "50%",
+    backgroundColor: "#e639478b",
+  },
 });
