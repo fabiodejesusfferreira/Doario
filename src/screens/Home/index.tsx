080d6f5d47170ef5c9ba8485fb9d141580cb518a
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-16 18:44:48 -0300

    feat: Importação e configuração dos Indicadores
    Dividido em renders

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,9 +3,62 @@ import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { Navigation } from "../../components/Navigation";
 import { Banner } from "../../components/Banner";
+import { Indicador } from "../../components/Indicador";
 
-export default function Home({ nav }: any) {
-  console.log(nav);
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  FontAwesome5,
+} from "@expo/vector-icons";
+
+const renderIndicadoresUltimasCampanhas = () => (
+  <View style={styles.indicadoresContainer}>
+    <Indicador
+      Icon={<Entypo name="megaphone" size={28} color={primaryColor} />}
+      title="Últimas campanhas"
+      description="Encontre e participe das últimas campanhas."
+      number={175}
+      numberText="campanhas em todo Brasil"
+    />
+  </View>
+);
+
+const renderIndicadoresTipoSanguineo = () => (
+  <View style={styles.indicadoresContainer}>
+    <Indicador
+      Icon={
+        <MaterialCommunityIcons
+          name="blood-bag"
+          size={28}
+          color={primaryColor}
+        />
+      }
+      title="Descubra seu tipo sanguíneo"
+      description="Faça o teste para descobrir seu tipo."
+      number={10567}
+      numberText="doadores cadastrados"
+    />
+  </View>
+);
+
+const renderIndicadoresHistorias = () => (
+  <View style={styles.indicadoresContainer}>
+    <Indicador
+      Icon={
+        <FontAwesome5
+          name="hand-holding-heart"
+          size={28}
+          color={primaryColor}
+        />
+      }
+      title="Histórias de quem foi salvo"
+      description="Veja vídeos de pessoas que foram salvas por conta da doação"
+      number={37235}
+      numberText="vidas"
+    />
+  </View>
+);
+
 
   return (
     <View style={styles.container}>
