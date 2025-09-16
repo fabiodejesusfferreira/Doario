9be5b3982377819a53aa4dd8396393e5beda5291
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-15 22:33:35 -0300

    feat: Criado o componente de navegação
    Nesse componente, tem um ícone para acessar o drawer navigation e a logo Doário centralizada

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Image } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import doarioLogo from "../../../assets/doario-logo-completa.png";
+import { useNavigation } from "@react-navigation/native";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { RootDrawerParamList } from "./props";
+
+import { styles } from "./styles";
+import { primaryColor } from "../../constants/palletColors";
+
+type NavigationProps = DrawerNavigationProp<RootDrawerParamList>;
+
+export function Navigation() {
+  const navigation = useNavigation<NavigationProps>();
+  function openDrawer() {
+    navigation.openDrawer();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Entypo name="menu" size={40} color={primaryColor} onPress={openDrawer} />
+      <Image
+        style={[styles.image, { alignSelf: "center" }]}
+        source={doarioLogo}
+      />
+    </View>
+  );
+}
---
+++ src/components/Navigation/styles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "97%",
+  },
+  image: {
+    marginHorizontal: "auto",
+    width: "40%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
