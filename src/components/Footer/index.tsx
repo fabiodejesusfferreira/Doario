f21fa88dba091357048136af7cfd0446cdfc1e1e
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-17 23:41:06 -0300

    feat: Itens dos links úteis agora podem navegar
    Exemplo: link para ver campanhas: leva para a screen de Campanhas

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Image, Text, SectionList } from "react-native";
 import DoarioLogoRodape from "../../../assets/doário-logo-rodape.png";
 import { DATA_LEGAL, DATA_LINKS_UTEIS } from "./data";
+import { useNavigation } from "@react-navigation/native";
+import { NavigationProps } from "../../types/navigation.types";
 
 import { styles, stylesHeader, stylesLinks, stylesCredits } from "./styles";
 
@@ -14,43 +16,85 @@ const Header = () => (
   </View>
 );
 
-const Links = () => (
-  <View style={stylesLinks.container}>
-    <SectionList
-      style={{ width: "50%", marginLeft: 20,  }}
-      inverted={true}
-      scrollEnabled={false}
-      sections={[{ title: "Links úteis", data: DATA_LINKS_UTEIS }]}
-      renderSectionFooter={({ section }) => (
-        <Text style={stylesLinks.sectionText}>{section.title}</Text>
-      )}
-      renderItem={({ item }) => (
-        <Text style={stylesLinks.itemText}>{item}</Text>
-      )}
-      keyExtractor={(item) => `links-${item}`}
-    />
+function Links() {
+  const navigation = useNavigation<NavigationProps>();
 
-    <SectionList
-      style={{  width: "50%", marginRight: 20 }}
-      inverted={true}
-      scrollEnabled={false}
-      sections={[{ title: "Legal e Contrato", data: DATA_LEGAL }]}
-      renderSectionFooter={({ section }) => (
-        <Text style={stylesLinks.sectionText}>{section.title}</Text>
-      )}
-      renderItem={({ item }) => (
-        <Text style={stylesLinks.itemText}>{item}</Text>
-      )}
-      keyExtractor={(item) => `links-${item}`}
-    />
-  </View>
-);
+  const handleNavigation = (itemName: string) => {
+    switch (itemName) {
+      case "Sobre o Doário+":
+        navigation.navigate("home");
+        break;
+
+      case "Campanhas":
+        navigation.navigate("campanhas");
+        break;
+
+      case "Educação":
+        navigation.navigate("campanhas");
+        break;
+
+      case "Histórias":
+        navigation.navigate("campanhas");
+        break;
+
+      case "Minha Jornada":
+        navigation.navigate("campanhas");
+        break;
+
+      default:
+        console.log(`Nenhuma rota definida para: ${itemName}`);
+        break;
+    }
+  };
+
+  return (
+    <View style={stylesLinks.container}>
+      <SectionList
+        style={{ width: "50%", marginLeft: 20 }}
+        inverted={true}
+        scrollEnabled={false}
+        sections={[{ title: "Links úteis", data: DATA_LINKS_UTEIS }]}
+        renderSectionFooter={({ section }) => (
+          <Text style={stylesLinks.sectionText}>{section.title}</Text>
+        )}
+        renderItem={({ item }) => (
+          <Text
+            onPress={() => handleNavigation(item)}
+            style={stylesLinks.itemText}
+          >
+            {item}
+          </Text>
+        )}
+        keyExtractor={(item) => `links-${item}`}
+      />
+
+      <SectionList
+        style={{ width: "50%", marginRight: 20 }}
+        inverted={true}
+        scrollEnabled={false}
+        sections={[{ title: "Legal e Contrato", data: DATA_LEGAL }]}
+        renderSectionFooter={({ section }) => (
+          <Text style={stylesLinks.sectionText}>{section.title}</Text>
+        )}
+        renderItem={({ item }) => (
+          <Text style={stylesLinks.itemText}>{item}</Text>
+        )}
+        keyExtractor={(item) => `links-${item}`}
+      />
+    </View>
+  );
+}
 
 const Credits = () => (
   <View style={stylesCredits.container}>
     <View style={stylesCredits.verticalLine}></View>
-    <Text style={stylesCredits.textCopyRight}>© 2025 Doário+. Todos os direitos reservados.</Text>
-    <Text style={stylesCredits.credits}>Site desenvolvido por <Text style={{ textDecorationLine:"underline" }}>@dejesusdev</Text></Text>
+    <Text style={stylesCredits.textCopyRight}>
+      © 2025 Doário+. Todos os direitos reservados.
+    </Text>
+    <Text style={stylesCredits.credits}>
+      Site desenvolvido por{" "}
+      <Text style={{ textDecorationLine: "underline" }}>@dejesusdev</Text>
+    </Text>
   </View>
 );
 
