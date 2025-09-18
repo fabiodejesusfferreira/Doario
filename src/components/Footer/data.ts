b476aa0ff001350d5a99185f5d6573bbdf055718
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-17 23:39:51 -0300

    raw: alteração na extensão do arquivo
    Extensão de TSX desnecessária, pois não estava tratando nenhum componente, apenas dados

---
+++ src/components/Footer/data.ts
@@ -0,0 +1,13 @@
+export const DATA_LINKS_UTEIS = [
+ "Sobre o Doário+",
+  "Campanhas",
+  "Educação",
+  "Histórias",
+  "Minha Jornada",
+].reverse();
+
+export const DATA_LEGAL = [
+  "Termos de Uso",
+  "Política de Privacidade",
+  "Contato",
+].reverse();
--- src/components/Footer/data.tsx
+++
@@ -1,7 +0,0 @@
-export const DATA_LINKS_UTEIS = [
-    "Sobre o Doário+", "Campanhas", "Educação", "Histórias", "Minha Jornada"
-].reverse();
-
-export const DATA_LEGAL = [
-    "Termos de Uso", "Política de Privacidade", "Contato"
-].reverse();
