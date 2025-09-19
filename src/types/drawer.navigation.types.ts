b2e65fb1b1335e884ca748534477b8a28dbffda6
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-19 09:22:29 -0300

    Adaptação das importações para o novo nome da tippagem

---
+++ src/types/drawer.navigation.types.ts
@@ -0,0 +1,4 @@
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { RootParamList } from "./root-param-list";
+
+export type NavigationDrawerProps = DrawerNavigationProp<RootParamList>;
--- src/types/navigation.types.ts
+++
@@ -1,4 +0,0 @@
-import { DrawerNavigationProp } from "@react-navigation/drawer";
-import { RootDrawerParamList } from "./root-drawer-param-list";
-
-export type NavigationProps = DrawerNavigationProp<RootDrawerParamList>;
