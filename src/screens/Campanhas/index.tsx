3c95755d2db61b616018404433011d370db18a72
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-22 22:59:32 -0300

    feat: Importação da toolbar

--- src/screens/Campanhas/index.tsx
+++ src/screens/Campanhas/index.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { View, Text, ScrollView } from "react-native";
 
 import { styles } from "./styles";
+import { ToolbarCampanhas } from "../../components/ToolbarCampanhas";
 
 export default function Campanhas() {
   return (
     <ScrollView style={{ flex: 1, }} showsVerticalScrollIndicator={false}>
       <View style={styles.container}>
         <Text style={styles.title}>Pesquisar por campanhas</Text>
+        <ToolbarCampanhas />
       </View>
     </ScrollView>
   );
