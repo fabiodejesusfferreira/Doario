995b40669273e8ed4a9d912fc14bb9845f881693
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-26 19:44:18 -0300

    feat: Ao clicar no texto inicial, ele abre o filter card

--- src/components/ListagemCampanhas/index.tsx
+++ src/components/ListagemCampanhas/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { useAnimation } from "../../contexts/AnimationProvider";
 
 import { styles } from "./styles";
 
 export function ListagemCampanhas() {
+  const { toggleCard } = useAnimation();
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.textContainer}>
-        <Text style={styles.text}>Utilize os filtros para pesquisar por campanhas!</Text>
+      <TouchableOpacity onPress={toggleCard} style={styles.textContainer}>
+        <Text style={styles.text}>
+          Utilize os filtros para pesquisar por campanhas!
+        </Text>
       </TouchableOpacity>
     </View>
   );
