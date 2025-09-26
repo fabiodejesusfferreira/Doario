4e1cd929a21b4b112265655a375af00a9b48f61b
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-26 19:40:38 -0300

    Estilização global para as seções do filter card

---
+++ src/components/FilterCard/globalStyles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  globalWidthContainer: {
+    width: "85%",
+  },
+  sectionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  sectionContainerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
